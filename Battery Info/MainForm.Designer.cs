929d425932b3b5db43c2e5df37a9cbfa5d7365f5
Joseph Mignone <jmig3066@gmail.com>
2022-01-20 08:14:08 -0500

    Put everything back

---
+++ Battery Info/MainForm.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace Battery_Info
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.autoRefreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.refreshToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(599, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.autoRefreshToolStripMenuItem,
+            this.saveFileToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // autoRefreshToolStripMenuItem
+            // 
+            this.autoRefreshToolStripMenuItem.CheckOnClick = true;
+            this.autoRefreshToolStripMenuItem.Name = "autoRefreshToolStripMenuItem";
+            this.autoRefreshToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.autoRefreshToolStripMenuItem.Text = "Auto-refresh";
+            this.autoRefreshToolStripMenuItem.Visible = false;
+            // 
+            // saveFileToolStripMenuItem
+            // 
+            this.saveFileToolStripMenuItem.Name = "saveFileToolStripMenuItem";
+            this.saveFileToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.saveFileToolStripMenuItem.Text = "Save Results";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(138, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(44, 20);
+            this.toolStripMenuItem1.Text = "&Help";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(107, 22);
+            this.toolStripMenuItem2.Text = "About";
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            // 
+            // treeView
+            // 
+            this.treeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView.Location = new System.Drawing.Point(0, 24);
+            this.treeView.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(599, 260);
+            this.treeView.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "xml";
+            this.saveFileDialog.FileName = "results.xml";
+            this.saveFileDialog.Filter = "Extensible Markup Language File|*.xml|Hypertext Markup Language|*.html";
+            this.saveFileDialog.Title = "Save Results";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 284);
+            this.Controls.Add(this.treeView);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Battery Info";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void AutoRefreshToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.ToolStripMenuItem autoRefreshToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem saveFileToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ Battery Info/MainForm.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Power;
+using Windows.System.Power;
+
+namespace Battery_Info
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            refreshToolStripMenuItem.Click += RefreshToolStripMenuItem_Click;
+            saveFileToolStripMenuItem.Click += SaveFileToolStripMenuItem_Click;
+            exitToolStripMenuItem.Click += ExitToolStripMenuItem_Click;
+            toolStripMenuItem2.Click += ToolStripMenuItem2_Click;
+            RefreshTreeView();
+        }
+
+        private void ToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            new About().ShowDialog();
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        async void RefreshTreeView()
+        {
+            int index = 0;
+            treeView.BeginUpdate();
+            var deviceInfo = await DeviceInformation.FindAllAsync(Battery.GetDeviceSelector());
+            treeView.Nodes.Clear();
+            foreach (DeviceInformation device in deviceInfo)
+            {
+                try
+                {
+                    var battery = await Battery.FromIdAsync(device.Id);
+                    var report = battery.GetReport();
+                    int batteryCharge = (int)Math.Round(((decimal)report.RemainingCapacityInMilliwattHours / (decimal)report.FullChargeCapacityInMilliwattHours) * 100, 0);
+                    int healthPercentage = (int)Math.Round((decimal)report.FullChargeCapacityInMilliwattHours / (decimal)report.DesignCapacityInMilliwattHours * 100, 0);
+                    treeView.Nodes.Add("Battery" + index.ToString(), "Battery " + index.ToString() + " (" + batteryCharge + "%)");
+                    treeView.Nodes[index].Nodes.Add("DeviceID", "Device ID: " + device.Id);
+                    treeView.Nodes[index].Nodes.Add("BatteryStatus", "Status: " + StatusToString(report.Status));
+                    if (report.Status == BatteryStatus.Charging)
+                    {
+                        treeView.Nodes[index].Nodes.Add("ChrgRate", "Charge rate: " + report.ChargeRateInMilliwatts.ToString() + " mW");
+                    }
+                    else if (report.Status == BatteryStatus.Discharging)
+                    {
+                        treeView.Nodes[index].Nodes.Add("ChrgRate", "Discharge rate: " + Math.Abs((decimal)report.ChargeRateInMilliwatts).ToString() + " mW");
+                    }
+                    treeView.Nodes[index].Nodes.Add("DesignCapacity", "Design capacity: " + report.DesignCapacityInMilliwattHours.ToString() + " mWh");
+                    treeView.Nodes[index].Nodes.Add("ChargeCapacity", "Full charge capacity: " + report.FullChargeCapacityInMilliwattHours.ToString() + " mWh");
+                    string batteryHealthSuffix = "";
+                    if (healthPercentage > 80)
+                    {
+                        batteryHealthSuffix = "Healthy";
+                    }
+                    if (healthPercentage <= 80)
+                    {
+                        batteryHealthSuffix = "Battery may not perform as well.";
+                    }
+                    if (healthPercentage <= 60)
+                    {
+                        batteryHealthSuffix = "Battery health is not good. Consider replacing soon.";
+                    }
+                    if (healthPercentage <= 55)
+                    {
+                        batteryHealthSuffix = "Battery is not healthy. Replace it soon.";
+                    }
+                    if (healthPercentage <= 45)
+                    {
+                        batteryHealthSuffix = "It's not recommended that you continue using this battery.";
+                    }
+                    if (healthPercentage <= 30)
+                    {
+                        batteryHealthSuffix = "Battery is bad. Replace now.";
+                    }
+                    treeView.Nodes[index].Nodes.Add("RemainingCapacity", "Remaining capacity: " + report.RemainingCapacityInMilliwattHours.ToString() + " mWh (" + batteryCharge + "%)");
+                    treeView.Nodes[index].Nodes.Add("BatHealth", "Battery health: " + healthPercentage.ToString() + "% (" + batteryHealthSuffix + ")");
+                    index++;
+                }
+                catch { /* Add error handling, as applicable */ }
+            }
+            treeView.ExpandAll();
+            treeView.EndUpdate();
+        }
+
+        private void SaveNodes(TreeNodeCollection nodesCollection, XmlWriter textWriter)
+        {
+            foreach (var node in nodesCollection.OfType<TreeNode>().Where(x => x.Nodes.Count == 0))
+                textWriter.WriteAttributeString(node.Name, node.Text);
+
+            foreach (var node in nodesCollection.OfType<TreeNode>().Where(x => x.Nodes.Count > 0))
+            {
+                textWriter.WriteStartElement(node.Name);
+                SaveNodes(node.Nodes, textWriter);
+                textWriter.WriteEndElement();
+            }
+        }
+
+        private void SaveNodes(TreeNodeCollection nodesCollection, StreamWriter htmlDocumentWriter)
+        {
+            foreach (var node in nodesCollection.OfType<TreeNode>().Where(x => x.Nodes.Count == 0))
+            {
+                // Write the node value as a paragraph
+                htmlDocumentWriter.WriteLine("<p style=\"margin: 0px; padding: 0px;\">" + node.Text + "</p>");
+            }
+
+            // Write info for each battery
+            foreach (var node in nodesCollection.OfType<TreeNode>().Where(x => x.Nodes.Count > 0))
+            {
+                // Battery name
+                htmlDocumentWriter.WriteLine("<h2 style=\"margin: 4px; padding: 0px;\">" + node.Text + "</h2>");
+                SaveNodes(node.Nodes, htmlDocumentWriter);
+            }
+        }
+
+        string StatusToString(BatteryStatus batteryStatus)
+        {
+            switch (batteryStatus)
+            {
+                case BatteryStatus.Charging:
+                    return "Charging";
+
+                case BatteryStatus.Discharging:
+                    return "Discharging";
+
+                case BatteryStatus.Idle:
+                    return "Idle";
+
+                case BatteryStatus.NotPresent:
+                    return "Not Present";
+
+                default:
+                    return "Unknown";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (autoRefreshToolStripMenuItem.Checked == true)
+            {
+                RefreshTreeView();
+            }
+        }
+
+        private void RefreshToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            RefreshTreeView();
+        }
+
+        private void SaveFileToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            SaveToFile();
+        }
+
+        void SaveToFile()
+        {
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string extension = Path.GetExtension(saveFileDialog.FileName).Substring(1);
+                switch (extension)
+                {
+                    case "xml":
+                        XmlWriterSettings settings = new XmlWriterSettings();
+                        settings.WriteEndDocumentOnClose = true;
+                        settings.Indent = true;
+                        settings.NewLineOnAttributes = true;
+                        XmlWriter xmlWriter = XmlWriter.Create(saveFileDialog.FileName, settings);
+                        SaveNodes(treeView.Nodes, xmlWriter);
+                        xmlWriter.Close();
+                        break;
+
+                    case "html":
+                        StreamWriter htmlDocument = new StreamWriter(saveFileDialog.FileName);
+                        htmlDocument.WriteLine("<!DOCTYPE html>");
+                        htmlDocument.WriteLine("<html>");
+                        htmlDocument.WriteLine("<head>");
+                        htmlDocument.WriteLine("<title>Battery Summary</title>");
+                        htmlDocument.WriteLine("</head>");
+                        htmlDocument.WriteLine("<body>");
+                        // Write current date and time
+                        htmlDocument.WriteLine("<p>Generated on " + DateTime.Now.ToString() + "</p>");
+                        htmlDocument.WriteLine("<hr>");
+                        htmlDocument.WriteLine("<h1>Battery Summary</h1>");
+                        htmlDocument.WriteLine("<hr>");
+                        SaveNodes(treeView.Nodes, htmlDocument);
+                        htmlDocument.WriteLine("</body>");
+                        htmlDocument.WriteLine("</html>");
+                        htmlDocument.Close();
+                        break;
+
+                    default:
+                        MessageBox.Show("Unsupported file type.");
+                        break;
+                }
+            }
+        }
+    }
+}
